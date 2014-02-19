ecbfbf3c8685ed41a8844f06a255823efff95d29
Itay Adler <itayadler@gmail.com>
2014-02-19 17:03:56 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InstantRubyShot::Application.config.secret_key_base = '08465ff0dafa4565174b4b8c871b7652379e53fe35c0e03de685d9bea4ae447649b681ecd94e6672ff78d29e59cae4ff107727951087d624399f5634737f07d6'
